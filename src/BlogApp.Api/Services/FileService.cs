0641e14e5a2cfa4b1f78d0a9145ffac04f31755e
Khushboqof <xushboqov.elbek@mail.ru>
2022-10-18 19:29:16 +0500

    upd

--- src/BlogApp.Api/Services/FileService.cs
+++ src/BlogApp.Api/Services/FileService.cs
@@ -30,6 +30,8 @@ namespace BlogApp.Api.Services
 
             var stream = File.Create(path);
             await file.CopyToAsync(stream);
+            stream.Close();
+
             return partPath;
         }
 
